7fda9524c1ae52a6d4a27588296cb7cb4262b731
Joseph Rex <joerexko@gmail.com>
2020-06-23 00:54:37 -0500

    Add markup for more prop options

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const InlineFormat = ({ action }) => (
-  <form action={action}>
+const InlineForm = ({
+  action,
+  submitText,
+  emailPlaceholder,
+  namePlaceholder,
+  hideName,
+  showLabels,
+  nameLabel,
+  emailLabel,
+  newTabSubmit,
+  }) => (
+  <form action={action} target={newTabSubmit ? '_blank' : 'self'}>
+    {!hideName && (
+      <>
+        {showLabels ? <label>{nameLabel}</label> : null}
+        <input type="text" name="fields[first_name]" placeholder={namePlaceholder} />
+      </>
+    )}
+    {showLabels ? <label>{emailLabel}</label> : null}
+    <input type="email" name="email_address" placeholder={emailPlaceholder} />
+    <button type="submit">{ submitText }</button>
   </form>
 )
 
 const formFormat = (format, props) => {
   const formatEmbeds = {
-    inline: () => <InlineFormat {...props} />,
+    inline: () => <InlineForm {...props} />,
     modal: () => null,
     slide: () => null,
     sticky: () => null,
@@ -26,6 +45,25 @@ const ConvertKitForm = ({ format = 'inline', formId, ...props }) => {
 ConvertKitForm.propTypes = {
   format: PropTypes.string.isRequired,
   formId: PropTypes.number.isRequired,
+  submitText: PropTypes.string,
+  emailPlaceholder: PropTypes.string,
+  namePlaceholder: PropTypes.string,
+  nameLabel: PropTypes.string,
+  emailLabel: PropTypes.string,
+  showLabels: PropTypes.bool,
+  hideName: PropTypes.bool,
+  newTabSubmit: PropTypes.bool,
+}
+
+ConvertKitForm.defaultProps = {
+  submitText: 'Submit',
+  emailPlaceholder: 'Your email',
+  namePlaceholder: 'Your first name',
+  nameLabel: 'First name',
+  emailLabel: 'Email',
+  showLabels: false,
+  hideName: false,
+  newTabSubmit: false,
 }
 
 export default ConvertKitForm
