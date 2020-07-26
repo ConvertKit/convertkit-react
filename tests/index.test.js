a2bf5fa617949f2d9eb8bc3fe705a5763ba51949
Joseph Rex <joerexko@gmail.com>
2020-07-26 07:31:21 -0500

    Reference new binaries from test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import ConvertKitForm from '../bin/convertkit-form'
+import ConvertKitForm from '../bin/convertkit-react.esm'
 
 const FORMID = 1234567
 
